35a3647b8eff119c4646431202eeb12a033acc53
coolmarvel <marvel97@naver.com>
2024-01-28 12:39:05 +0900

    next app basic config setting

---
+++ article-project/src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Article {
+  editor: string;
+  title: string;
+  keyword?: string;
+  description?: string;
+  mainImageUrl?: string;
+  avatarImageUrl?: string;
+  link: string;
+}
+
+export interface Keyword {
+  keyword: string;
+}
